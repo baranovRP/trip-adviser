1ce9d24b15bee788cca4bc82d626af787c6c6d42
baranovrp <baranov.r.p@gmail.com>
2019-06-28 22:05:49 +0200

    TA-002 Create Entities

---
+++ germes-model/src/main/java/org/baranov/germes/app/model/entity/base/AbstractEntity.java
@@ -0,0 +1,107 @@
+package org.baranov.germes.app.model.entity.base;
+
+import org.baranov.germes.app.model.entity.person.Account;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+/**
+ * Base class for all business entities
+ */
+public abstract class AbstractEntity {
+    /**
+     * Unique entity identifier
+     */
+    private int id;
+
+    /**
+     * Timestamp of entity creation
+     */
+    private LocalDateTime createdAt;
+
+    /**
+     * Timestamp of entity last modification
+     */
+    private LocalDateTime modifiedAt;
+
+    /**
+     * Person who created specific entity
+     */
+    private Account createdBy;
+
+    /**
+     * Last person who modified entity
+     */
+    private Account modifiedBy;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(LocalDateTime modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+
+    public Account getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Account createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Account getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(Account modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AbstractEntity that = (AbstractEntity) o;
+
+        return Objects.equals(this.id, that.id) &&
+                Objects.equals(this.createdAt, that.createdAt) &&
+                Objects.equals(this.createdBy, that.createdBy) &&
+                Objects.equals(this.modifiedAt, that.modifiedAt) &&
+                Objects.equals(this.modifiedBy, that.modifiedBy);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", this.getClass().getSimpleName() + "[", "]")
+                .add("id = " + id)
+                .add("createdAt = " + createdAt)
+                .add("createdBy = " + createdBy)
+                .add("modifiedAt = " + modifiedAt)
+                .add("modifiedBy = " + modifiedBy)
+                .toString();
+    }
+}
---
+++ germes-model/src/main/java/org/baranov/germes/app/model/entity/person/Account.java
@@ -0,0 +1,10 @@
+package org.baranov.germes.app.model.entity.person;
+
+import org.baranov.germes.app.model.entity.base.AbstractEntity;
+
+/**
+ * Entity that encapsulates user of the application
+ */
+public class Account extends AbstractEntity {
+
+}
