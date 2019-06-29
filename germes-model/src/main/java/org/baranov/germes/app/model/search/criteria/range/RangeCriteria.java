35ec5fdac8d77699c0f7113bc3cd8b890dc47297
baranovrp <baranov.r.p@gmail.com>
2019-06-29 22:12:46 +0200

    TA-006 Add Search criteria

---
+++ germes-model/src/main/java/org/baranov/germes/app/model/search/criteria/range/RangeCriteria.java
@@ -0,0 +1,29 @@
+package org.baranov.germes.app.model.search.criteria.range;
+
+/**
+ * Pagination parameters for data retrieval operations
+ */
+public class RangeCriteria {
+    /**
+     * Page index(0-based)
+     */
+    private final int page;
+
+    /**
+     * Number of elements per page
+     */
+    private final int rowCount;
+
+    public RangeCriteria(final int page, final int rowCount) {
+        this.page = page;
+        this.rowCount = rowCount;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public int getRowCount() {
+        return rowCount;
+    }
+}
