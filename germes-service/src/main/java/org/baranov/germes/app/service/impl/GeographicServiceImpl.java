d476da32ed87de51e5103e2624d2a4477ae1d5c3
baranovrp <baranov.r.p@gmail.com>
2019-07-11 21:02:44 +0200

    ITSIM-0085 Add InMemoryCityRepository

--- germes-service/src/main/java/org/baranov/germes/app/service/impl/GeographicServiceImpl.java
+++ germes-service/src/main/java/org/baranov/germes/app/service/impl/GeographicServiceImpl.java
@@ -1,67 +1,51 @@
 package org.baranov.germes.app.service.impl;
 
-import org.baranov.germes.app.infra.util.CommonUtil;
 import org.baranov.germes.app.model.entity.geography.City;
 import org.baranov.germes.app.model.entity.geography.Station;
 import org.baranov.germes.app.model.search.criteria.StationCriteria;
 import org.baranov.germes.app.model.search.criteria.range.RangeCriteria;
+import org.baranov.germes.app.persistence.repository.CityRepository;
+import org.baranov.germes.app.persistence.repository.inmemory.InMemoryCityRepository;
 import org.baranov.germes.app.service.GeographicService;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
  * Default implementation of the {@link GeographicService}
  */
 public class GeographicServiceImpl implements GeographicService {
-    /**
-     * Internal list of cities
-     */
-    private final List<City> cities;
 
-    /**
-     * Auto-increment counter for entity id generation
-     */
-    private int counter = 0;
-
-    private int stationCounter = 0;
+    private final CityRepository cityRepository;
 
     public GeographicServiceImpl() {
-        cities = new ArrayList<>();
+        cityRepository = new InMemoryCityRepository();
     }
 
     @Override
     public List<City> findCities() {
-        return CommonUtil.getSafeList(cities);
+        return cityRepository.findAll();
     }
 
     @Override
-    public void saveCity(final City city) {
-        if (!cities.contains(city)) {
-            city.setId(++counter);
-            cities.add(city);
-        }
-        city.getStations().forEach(station -> {
-            if (station.getId() == 0) {
-                station.setId(++stationCounter);
-            }
-        });
+    public void saveCity(City city) {
+        cityRepository.save(city);
     }
 
     @Override
     public Optional<City> findCityById(final int id) {
-        return cities.stream().filter(city -> city.getId() == id).findAny();
+        return Optional.ofNullable(cityRepository.findById(id));
     }
 
     @Override
     public List<Station> searchStations(final StationCriteria criteria, final RangeCriteria rangeCriteria) {
+        Set<Station> stations = new HashSet<>();
 
-        return cities.stream()
-                .flatMap(city -> city.getStations().stream())
-                .collect(Collectors.toSet()).stream()
-                .filter(station -> station.match(criteria))
-                .collect(Collectors.toList());
+        cityRepository.findAll().forEach(city -> stations.addAll(city.getStations()));
+
+        return stations.stream().filter(station -> station.match(criteria)).collect(Collectors.toList());
     }
 }
